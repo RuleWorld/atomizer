cd052b26ed3603c78b9053eb05e8b748e5c2a4b8
Robert Clark <rhclark.pitt@gmail.com>
2017-07-30 16:43:50 -0400

    make curl statement verbose

--- .curl_ftp.sh
+++ .curl_ftp.sh
@@ -1,8 +1,8 @@
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  curl -T ./dist/sbmlTranslator-$TRAVIS_OS_NAME               -u $FTP_USER_4:$FTP_PASSWORD ftp://ftp.drivehq.com/d_data/d_travis/  
+  curl -v -T ./dist/sbmlTranslator-$TRAVIS_OS_NAME               -u $FTP_USER_4:$FTP_PASSWORD ftp://ftp.drivehq.com/d_data/d_travis/  
 else
-  curl -T ./dist/sbmlTranslator-$TRAVIS_OS_NAME               -u $FTP_USER_5:$FTP_PASSWORD ftp://ftp.drivehq.com/d_data/d_travis/  
+  curl -v -T ./dist/sbmlTranslator-$TRAVIS_OS_NAME               -u $FTP_USER_5:$FTP_PASSWORD ftp://ftp.drivehq.com/d_data/d_travis/  
 fi
 
 
