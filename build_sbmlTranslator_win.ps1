3c5720d506acf6094d01aad7cc09d63acae03990
Jacob Czech <czech.jacob@gmail.com>
2018-04-17 20:15:39 -0400

    Add powershell script for building in Windows.

---
+++ build_sbmlTranslator_win.ps1
@@ -0,0 +1,10 @@
+﻿cd .\SBMLparser
+# You must add Python to your path for this to work.
+# This is pretty easy to do with Anaconda
+python -m venv .\atomizer_venv
+.\atomizer_venv\Scripts\Activate.ps1
+pip install --upgrade pip
+pip install -r .\requirements.txt
+pip install --target=. python-libsbml
+pyinstaller .\sbmlTranslator.spec
+deactivate
