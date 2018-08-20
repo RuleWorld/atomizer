d7de27b972f73b3fa070739916b81fdc48ba48d0
Jacob Czech <czech.jacob@gmail.com>
2018-08-20 14:24:01 -0400

    Bugfix: Travis OSX build not finding PyInstaller
    It's either not creating the PyInstaller executable or adding it to the
    path. According to the Travis logs, PyInstaller itself is being
    installed correctly, so I'm just going to try to invoke it via a
    "python3 -m PyInstaller" instead.
    
    There seems to be a problem with numpy on osx that I'll tackle next.

--- build_sbmlTranslator.sh
+++ build_sbmlTranslator.sh
@@ -6,5 +6,5 @@ source atomizer_venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install --target=. python-libsbml
-pyinstaller sbmlTranslator.spec
+python3 -m PyInstaller sbmlTranslator.spec
 deactivate
